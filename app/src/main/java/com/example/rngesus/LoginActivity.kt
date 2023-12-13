0bf3812547ca97bb975593418bce848c67b2df5d
Yuyang Huang <yuyang97@bu.edu>
2023-12-13 13:28:54 -0500

    adding icon

--- app/src/main/java/com/example/rngesus/LoginActivity.kt
+++ app/src/main/java/com/example/rngesus/LoginActivity.kt
@@ -1,13 +1,13 @@
 package com.example.rngesus
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
-import com.example.rngesus.RegisterActivity
+
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var  auth: FirebaseAuth
